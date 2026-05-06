10abac8b4031fd8a1c64c4135bc288d1d6951973
Swagi-REDACTED <111149273+Swagi-REDACTED@users.noreply.github.com>
2026-05-06 14:59:38 -0400

    Delete src directory

--- src/main/java/net/blupillcosby/furnacexptweaks/access/SlotExtra.java
+++
@@ -1,6 +0,0 @@
-package net.blupillcosby.furnacexptweaks.access;
-
-public interface SlotExtra {
-    boolean furnaceXpTweaks$isOutput();
-    void furnaceXpTweaks$setOutput(boolean value);
-}
--- src/main/java/net/blupillcosby/furnacexptweaks/access/TechRebornExperienceAccessor.java
+++
@@ -1,6 +0,0 @@
-package net.blupillcosby.furnacexptweaks.access;
-
-public interface TechRebornExperienceAccessor {
-    float furnaceXpTweaks$getExperience();
-    void furnaceXpTweaks$setExperience(float experience);
-}
